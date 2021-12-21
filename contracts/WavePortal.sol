e950c14407e3c8aa9c5772a3923170ddf5500d3f
Rodrigo Villegas <rodrigo.villegas.venegas@gmail.com>
2021-12-21 00:50:29 -0500

    Done

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+
+
+// mapping of addresses and arrays to have users and their recommendations
+    //length of arrays will be number of contributions
+//mapping of strings and ints
+    //strings will be books and ints will be the number of recommendations
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalSubmitions;
+
+    mapping(string => uint) public popularity; //update later to actually store books and popularity
+
+    uint256 private seed;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable{
+        console.log("Constructed.");
+
+        seed = (block.timestamp + block.difficulty) % 100;
+    
+    }
+
+    function wave(string memory _message) public {
+
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 15m"
+        );
+        lastWavedAt[msg.sender] = block.timestamp;
+        totalSubmitions += 1;
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        
+        console.log("Random # generated: %d", seed);
+
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalSubmitions);
+        return totalSubmitions;
+    }
+}
